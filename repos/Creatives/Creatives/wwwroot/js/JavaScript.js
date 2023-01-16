bdb6f74e376b7dd7d1b962c360415034fcf0e4da
ahmedalfarsi <alfarsi.omani@gmail.com>
2023-01-16 13:54:33 +0400

    first commit

---
+++ repos/Creatives/Creatives/wwwroot/js/JavaScript.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+    $("#btnSummer").click(function () {
+        $(".blue-bg").addClass("summer");
+        $(".blue-bg").removeClass("winter");
+    });
+    $("#btnWinter").click(function () {
+        $(".blue-bg").addClass("winter");
+        $(".blue-bg").removeClass("summer");
+    });
+    $("#btnDefault").click(function () {
+        $(".blue-bg").removeClass("winter");
+        $(".blue-bg").removeClass("summer");
+    });
+
+    $("#home").click(function () {
+
+        $("html,body").animate({
+            "scrollTop": "0"
+        });
+    });
+
+    $("#pages").click(function () {
+        
+        $('html,body').animate({
+            "scrollTop": "879px"
+        });
+    });
+    $("#ftr").click(function () {
+
+        $('html,body').animate({
+            "scrollTop": "1624px"
+        });
+    });
+
+    $("#ext").click(function () {
+        
+        $('html,body').animate({
+            "scrollTop": "2754px"
+        });
+    });
+
+    $("#tut").click(function () {
+
+        $('html,body').animate({
+            "scrollTop": "3531px"
+        });
+    });
+
+    $("#conct").click(function () {
+
+        $('html,body').animate({
+            "scrollTop": "5074x"
+        });
+    });
+});
